4a8b1f1030bf02b496a4228fecda034be3caa9e3
Eric Freudenthal <eric@freudenthal.net>
2023-09-06 00:50:03 -0600

    made cpu.py more similar to code from remzi

--- ch2/cpu.py
+++ ch2/cpu.py
@@ -1,19 +1,23 @@
 #! /usr/bin/env python3
 
-from os import getpid
-from sys import argv
-from time import time
 
-global val
-val = int(argv[1])
+from os import getpid
+from sys import argv, stderr, exit
+from time import time, sleep
+
+if len(argv) != 2:
+    print (f"USAGE: {argv[0]} message", file=stderr)
+    exit(1)
+
+
+str = argv[1]
 pid = getpid()
 
 def delay(sec):
     waitUntil = time() + sec
     while (time() < waitUntil):
-        pass
+        pass 
 
 for t in range(5):
-    print("(pid=%d) val=%d" % (pid, val))
+    print("(pid=%d) %s" % (pid, str))
     delay(1)
-    val += 1
