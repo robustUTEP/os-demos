7d4e3ca38b7607c68dac7553e03f2466795f0e24
Eric Freudenthal <eric@freudenthal.net>
2023-09-11 10:31:26 -0600

    used raw io and sys.stderr properly in io.py

--- ch2/io.py
+++ ch2/io.py
@@ -1,14 +1,19 @@
 #! /usr/bin/env python3
 
 import os, stat
+from sys import stderr
 
 def dowork():
+    totalBytesWritten = 0
     fd = os.open("/tmp/file", os.O_WRONLY | os.O_CREAT | os.O_TRUNC, stat.S_IRWXU);
     assert fd >= 0
     buffer = "hello world\n"
-    rc = os.write(fd, buffer.encode())
-    assert rc == len(buffer)
-    os.write(1, ("wrote %s bytes\n" % rc).encode())
+    while len(buffer):
+        wc = os.write(fd, buffer.encode())
+        buffer = buffer[wc:]
+        os.write(1, ("wrote %s bytes\n" % wc).encode())
+        totalBytesWritten += wc
+    print(f"wrote a total of {totalBytesWritten} bytes\n", file=stderr)
     os.fsync(fd)
     os.close(fd)
 
