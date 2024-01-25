2e2546a4b6dda959de910d225558a0d527b733d1
Eric Freudenthal <eric@freudenthal.net>
2024-01-25 08:50:42 -0700

    used write syscall to write to stdin & stderr

--- ch2/io.py
+++ ch2/io.py
@@ -1,19 +1,21 @@
 #! /usr/bin/env python3
 
 import os, stat
-from sys import stderr
+from sys import stderr, argv
+
+infile=argv[1]
 
 def dowork():
     totalBytesWritten = 0
-    fd = os.open("/tmp/file", os.O_WRONLY | os.O_CREAT | os.O_TRUNC, stat.S_IRWXU);
+    fd = os.open(infile, os.O_WRONLY | os.O_CREAT | os.O_TRUNC, stat.S_IRWXU);
     assert fd >= 0
-    buffer = "hello world\n"
+    buffer = "hello world\n".encode()
     while len(buffer):
-        wc = os.write(fd, buffer.encode())
+        wc = os.write(fd, buffer)
         buffer = buffer[wc:]
         os.write(1, ("wrote %s bytes\n" % wc).encode())
         totalBytesWritten += wc
-    print(f"wrote a total of {totalBytesWritten} bytes\n", file=stderr)
+    os.write(2, f"wrote a total of {totalBytesWritten} bytes\n".encode())
     os.fsync(fd)
     os.close(fd)
 
