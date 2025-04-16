b6e1281c47dfa70c0db406b08c207c7fd9078bfe
Eric Freudenthal <eric@freudenthal.net>
2025-04-16 10:10:52 -0600

    added much annotation and logging

--- ch2/threads_v0.py
+++ ch2/threads_v0.py
@@ -2,7 +2,7 @@
 
 from sys import argv
 from time import time, sleep
-from threading import Thread, enumerate
+from threading import Thread, enumerate, current_thread
 
 global count
 count = 0
@@ -11,27 +11,44 @@ itersPerThread = int(argv[1])
 
 threadNum = 0
 
+def add(a, b):
+    return a+b
+
+
+
 class Worker(Thread):
     def __init__(self, iters):
-        global threadNum
-        Thread.__init__(self, name="Thread-%d" % threadNum);
-        threadNum += 1
+        Thread.__init__(self)
         self.iters = iters
+        print(f"--{self.name} created")
     def run(self):
         global count
+        print(f"--{self.name} started")
         for i in range(self.iters):
-            count += 1
+            count = add(count, 1)
+        print(f"--{self.name} exiting")
+
+
+
+
+print(f"Main thread's name is {current_thread().name}")
+# enumerate() retuns the set of live threads
+print(f"Number of threads before any workers created = {len(enumerate())}")
+
+wallClockStart = time()
 
 
 workers = [ Worker(itersPerThread) for i in range(2) ]
 
+
 for worker in workers:
     worker.start()
 
+while (nLiveThreads := len(enumerate())) > 1: # while at least one worker alive
+    print(f"#threads still alive = {nLiveThreads}; count = {count}")
+    sleep(0.1) #    delay(0.25)
 
-while len(enumerate()) > 1:
-    sleep(0.25)
-    print("#activeThreads=%d, count=%d" % (len(enumerate()), count))
-print(count)
+print(f"final count = {count}, total wall clock time = {time() - wallClockStart:.2}s")
+
 
     
--- ch2/threads_v1.py
+++ ch2/threads_v1.py
@@ -2,40 +2,50 @@
 
 from sys import argv
 from time import time, sleep
-from threading import Thread, enumerate, Lock
+from threading import Thread, enumerate, Lock, current_thread
 
 global count
 count = 0
-global countLock
-countLock = Lock()
+countLock = Lock()              # resource proxy for count
 
 itersPerThread = int(argv[1])
 
 threadNum = 0
 
+def add(a, b):
+    return a + b
+
+
 
 class Worker(Thread):
     def __init__(self, iters):
-        global threadNum
-        Thread.__init__(self, name="Thread-%d" % threadNum)
-        threadNum += 1
+        Thread.__init__(self)
         self.iters = iters
+        print(f"--{self.name} created")
     def run(self):
         global count, countLock
+        print(f"--{self.name} started")
         for i in range(self.iters):
-            countLock.acquire()
-            count += 1
-            countLock.release()
+            countLock.acquire() # acquire count resource
+            count = add(count, 1)
+            countLock.release() # release count resource
+        print(f"--{self.name} exiting")
 
+print(f"Main thread's name is {current_thread().name}")
+# enumerate() retuns the set of live threads
+print(f"Number of threads before any workers created = {len(enumerate())}")
+
+wallClockStart = time()
 
 workers = [ Worker(itersPerThread) for i in range(2) ]
 
 for worker in workers:
     worker.start()
 
-while len(enumerate()) > 1:
+while (nLiveThreads := len(enumerate())) > 1:     # while at least one worker alive...
+    print(f"#threads still alive = {nLiveThreads}; count = {count}")
     sleep(0.25) #    delay(0.25)
-    print("#activeThreads=%d, count=%d" % (len(enumerate()), count))
-print(count)
+
+print(f"final count = {count}, total wall clock time = {time() - wallClockStart:.2}s")
 
     
